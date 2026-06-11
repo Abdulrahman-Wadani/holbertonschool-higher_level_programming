ad572f932dd7cb284f18d1a87f657bf278efa147
Abdulrahman-Wadani <abdulrahmanqmb@gmail.com>
2026-06-11 17:32:05 +0300

    task 5

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-def no_c(my_string=str):
-    my_string = my_string.replace("c", "")
-    my_string = my_string.replace("C", "")
+def no_c(my_string):
+    for i in range(0, len(my_string)):
+        if i == 0 and (my_string[i] == "C" or my_string[i] == "c"):
+            my_string = my_string[1:]
+        elif i < len(my_string) and (my_string[i] == "c" or my_string[i] == "C"):
+            my_string = my_string[0:i] + my_string[i + 1:]
     return my_string
