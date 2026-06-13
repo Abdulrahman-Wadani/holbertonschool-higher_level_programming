0c4b4035445579d3c5c3fc4ab37cc425f0507490
Abdulrahman-Wadani <abdulrahmanqmb@gmail.com>
2026-06-13 17:33:46 +0300

    task 6

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -4,4 +4,4 @@ def print_matrix_integer(matrix=[[]]):
         if len(i) == 0:
             print()
         for j in range(0, len(i)):
-            print("{:d}".format(i[j]), end=" " if j != i[len(i) - 1] else "\n")
+            print("{:d}".format(i[j]), end=" " if j != len(i) - 1 else "\n")
