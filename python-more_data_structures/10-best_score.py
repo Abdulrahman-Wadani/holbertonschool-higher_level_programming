9dbe6781f2cea75f51ad37dcd2a19d681b6ca438
Abdulrahman-Wadani <abdulrahmanqmb@gmail.com>
2026-06-23 16:05:00 +0300

    task 10

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+def best_score(a_dictionary=dict):
+    if not a_dictionary or len(a_dictionary) == 0:
+        return None
+    key = ""
+    val = 0
+    for i in a_dictionary.keys():
+        if a_dictionary[i] > i:
+            key = a_dictionary[i]
+    return key
