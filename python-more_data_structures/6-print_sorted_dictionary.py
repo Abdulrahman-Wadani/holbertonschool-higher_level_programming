fe77b3d0bfb2b9e76faf852d15759a6d84984aa9
Abdulrahman-Wadani <abdulrahmanqmb@gmail.com>
2026-06-23 15:01:21 +0300

    task 6

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -3,6 +3,3 @@ def print_sorted_dictionary(a_dictionary):
     new = sorted(dict.keys(a_dictionary))
     for i in new:
         print(i, dict.get(a_dictionary, i), sep=": ")
-
-a_dictionary = { 'language': "C", 'Number': 89, 'track': "Low level", 'ids': [1, 2, 3] }
-print_sorted_dictionary(a_dictionary)
