e70f2d9259f2f50204d9bf1db30a1abe4070e691
Abdulrahman-Wadani <abdulrahmanqmb@gmail.com>
2026-06-23 15:59:05 +0300

    task 9

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,1 +1,6 @@
 #!/usr/bin/python3
+def multiply_by_2(a_dictionary=dict):
+    a_dictionary = a_dictionary.copy()
+    key = a_dictionary.keys()
+    for i in key:
+        a_dictionary[i] = a_dictionary[i] * 2
