67031f21ab7a384e7adc82740ad5b77239a1f182
Abdulrahman-Wadani <abdulrahmanqmb@gmail.com>
2026-06-24 16:41:24 +0300

    task 4

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -6,20 +6,16 @@ class Square:
     """ square """
 
     def __init__(self, size=0):
-        if type(size) is not int:
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = size
+        self.size = size
 
     def area(self):
         return self.__size * self.__size
 
-    @getattr
+    @property
     def size(self):
         return self.__size
 
-    @setattr
+    @size.setter
     def size(self, value):
         if type(value) is not int:
             raise TypeError("size must be an integer")
