109530d407e8c1e9284f0a4774496d4f90cd5eab
Abdulrahman-Wadani <abdulrahmanqmb@gmail.com>
2026-06-24 14:52:09 +0300

    task 3

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+def safe_print_division(a, b):
+    num = None
+    try:
+        num = a / b
+    except Exception:
+        return num
+    finally:
+        print("Inside result: {}".format(num))
+    return num
