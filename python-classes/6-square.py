93c66a3540f21b97fd4e3bb7b4ef67244278be3c
Abdulrahman-Wadani <abdulrahmanqmb@gmail.com>
2026-06-24 17:15:17 +0300

    task 6

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -1,1 +1,64 @@
 #!/usr/bin/python3
+""" square """
+
+
+class Square:
+    """ square """
+
+    def __init__(self, size=0, position=(0, 0)):
+        self.size = size
+        self.position = position
+
+    def area(self):
+        return self.__size * self.__size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def my_print(self):
+        if self.size == 0:
+            print()
+        else:
+            for i in range(self.position[1]):
+                print()
+            for i in range(self.size):
+                for i in range(self.position[0]):
+                    print(" ", end="")
+                for i in range(self.size):
+                    print("#", end="")
+                print()
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if type(value) is not tuple or len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+
+my_square_1 = Square(3)
+my_square_1.my_print()
+
+print("--")
+
+my_square_2 = Square(3, (1, 1))
+my_square_2.my_print()
+
+print("--")
+
+my_square_3 = Square(3, (3, 0))
+my_square_3.my_print()
+
+print("--")
