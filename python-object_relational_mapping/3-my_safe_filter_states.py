625fc8dbfb4652d7c164d4dcc6868d8ef9656228
Abdulrahman-Wadani <abdulrahmanqmb@gmail.com>
2026-07-21 14:07:54 +0300

    task 4

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -19,13 +19,9 @@ if __name__ == "__main__":
 
     cur = conn.cursor()
 
-    query = (
-        "SELECT cities.id, cities.name, states.name "
-        "FROM cities "
-        "JOIN states ON cities.state_id = states.id "
-        "ORDER BY cities.id ASC"
-    )
-    cur.execute(query)
+    cur.execute("SELECT cities.id, cities.name, states.name "
+                "FROM states JOIN cities ON cities.state_id = states.id "
+                "ORDER BY cities.id ASC")
 
     query_rows = cur.fetchall()
     for row in query_rows:
