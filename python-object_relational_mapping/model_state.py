4072ee65c09c8bea97e4fad95d20d654f2b1024d
Abdulrahman-Wadani <abdulrahmanqmb@gmail.com>
2026-07-21 19:53:59 +0300

    task 6

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -13,5 +13,5 @@ class State(Base):
     Class State that inherits from Base and links to the MySQL table 'states'
     """
     __tablename__ = 'states'
-    id = Column(Integer, primary_key=True, autoincrement=True)
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
