085304c8c8ddb8fb93c150030c115e8f6259b66b
Abdulrahman-Wadani <abdulrahmanqmb@gmail.com>
2026-07-21 13:38:18 +0300

    task 1

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,1 +1,26 @@
 #!/usr/bin/python3
+"""
+Lists all states from the database hbtn_0e_0_usa.
+Usage: ./0-select_states.py <mysql_username> <mysql_password> <database_name>
+"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        charset="utf8",
+    )
+    cur: MySQLdb.cursors.Cursor = conn.cursor()
+    cur.execute(
+        "SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC")
+    data = cur.fetchall()
+    for row in data:
+        print(row)
+    cur.close()
+    conn.close()
