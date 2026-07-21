fb98d3d604b1469d9b6228a719f301238e6e9aaf
Abdulrahman-Wadani <abdulrahmanqmb@gmail.com>
2026-07-21 14:05:35 +0300

    task 4

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """
-Lists all cities from the database hbtn_0e_0_usa.
+Lists all cities from the database hbtn_0e_4_usa.
 Usage: ./4-cities_by_state.py <mysql_username> <mysql_password> <database_name>
 """
 import MySQLdb
 import sys
+
 
 if __name__ == "__main__":
     conn = MySQLdb.connect(
@@ -13,21 +14,21 @@ if __name__ == "__main__":
         user=sys.argv[1],
         passwd=sys.argv[2],
         db=sys.argv[3],
-        charset="utf8",
+        charset="utf8"
     )
 
-    cur: MySQLdb.cursors.Cursor = conn.cursor()
+    cur = conn.cursor()
 
-    # 3. تنفيذ استعلام الـ JOIN بطريقة الدمج النظيف بدون مسافات زائدة
-    cur.execute(
+    query = (
         "SELECT cities.id, cities.name, states.name "
-        "FROM states "
-        "JOIN cities ON cities.state_id = states.id "
+        "FROM cities "
+        "JOIN states ON cities.state_id = states.id "
         "ORDER BY cities.id ASC"
     )
+    cur.execute(query)
 
-    data = cur.fetchall()
-    for row in data:
+    query_rows = cur.fetchall()
+    for row in query_rows:
         print(row)
 
     cur.close()
