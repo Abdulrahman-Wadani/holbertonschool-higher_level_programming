3fdefad1eb7198489cde57ef55acd552e018673d
Abdulrahman-Wadani <abdulrahmanqmb@gmail.com>
2026-07-22 11:30:38 +0300

    task 10

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -1,1 +1,30 @@
 #!/usr/bin/python3
+"""
+Lists all State objects from the database hbtn_0e_6_usa
+"""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import State, Base
+
+if __name__ == "__main__":
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(
+            sys.argv[1],
+            sys.argv[2],
+            sys.argv[3]
+        ),
+        pool_pre_ping=True
+    )
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = session.query(State).filter_by(
+        name=f'{sys.argv[4]}').order_by(State.id).first()
+
+    if state:
+        print(state.id)
+    else:
+        print("Not found")
+
+    session.close()
