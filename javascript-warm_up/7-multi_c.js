90b3efbb20820e8433e89e908d5292e25c499582
Abdulrahman-Wadani <abdulrahmanqmb@gmail.com>
2026-08-18 11:15:45 +0300

    task

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+const arr = process.argv;
+
+if (!Number.isNaN(Number(arr[2]))) {
+  for (let i = 0; i < (Number(arr[2]) | 0); i++) {
+    console.log('C is fun');
+  }
+} else {
+  console.log('Missing number of occurrences');
+}
