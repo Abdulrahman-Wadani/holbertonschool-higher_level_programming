8c758408e08b4cfc290d4270f0337244d8d5c2bf
Abdulrahman-Wadani <abdulrahmanqmb@gmail.com>
2026-08-18 10:58:19 +0300

    task

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,1 +1,9 @@
 #!/usr/bin/node
+const arr = process.argv;
+
+arr[2] -= 0;
+if (typeof arr[2] === 'number') {
+  console.log('My number: ' + (arr[2] | 0));
+} else {
+  console.log('Not a number');
+}
