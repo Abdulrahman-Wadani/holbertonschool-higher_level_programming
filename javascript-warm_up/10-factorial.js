25b24c301ea3f2e685e579316af93e4fd59797ea
Abdulrahman-Wadani <abdulrahmanqmb@gmail.com>
2026-08-18 11:41:31 +0300

    task

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+function factorial (a) {
+  if (Number.isNaN(a) || a <= 1) {
+    return 1;
+  }
+  return factorial(a - 1) * a;
+}
+
+const arr = process.argv;
+
+console.log(factorial(Number(arr[2])));
