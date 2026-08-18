9b284acbce912787e41449d2318da3c5edf8e563
Abdulrahman-Wadani <abdulrahmanqmb@gmail.com>
2026-08-18 11:04:03 +0300

    task

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
 const arr = process.argv;
 
-arr[2] -= 0;
-if (typeof arr[2] === 'number') {
+if (!Number.isNaN(Number(arr[2]))) {
   console.log('My number: ' + (arr[2] | 0));
 } else {
   console.log('Not a number');
