41c6daf410ee5ee16f934c6e6b75ce175f910ad1
Abdulrahman-Wadani <abdulrahmanqmb@gmail.com>
2026-08-18 11:58:39 +0300

    task

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
 const arr = process.argv;
 
-arr.sort();
-arr.reverse();
+arr.sort((a, b) => b - a);
 
 if (arr.length < 4) {
   console.log(0);
 } else {
-  console.log(arr[1]);
+  console.log(arr[3]);
 }
