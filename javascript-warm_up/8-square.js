1af3b29fbc5144400420b4e4d12a545233977fab
Abdulrahman-Wadani <abdulrahmanqmb@gmail.com>
2026-08-18 11:25:34 +0300

    task

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -1,1 +1,15 @@
 #!/usr/bin/node
+const arr = process.argv;
+
+let str = "";
+if (!Number.isNaN(Number(arr[2]))) {
+  for (let i = 0; i < (Number(arr[2]) | 0); i++) {
+    for (let j = 0; j < (Number(arr[2]) | 0); j++) {
+      str += 'X';
+    }
+    str += '\n';
+  }
+  console.log(str);
+} else {
+    console.log('Missing size');
+}
